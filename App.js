7f13ea152e4934037c1d6e95407295ebf2356b0b
Gligorije <gligorije.nikolic989@gmail.com>
2018-12-12 12:26:34 +0100

    Connectivity indicator

--- App.js
+++ App.js
@@ -1,13 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import Status from './components/Status';
 
 export default class App extends React.Component {
+  renderMessageList() {
+    return <View style={styles.content} />;
+  }
+
+  renderInputMethodEditor() {
+    return <View style={styles.inputMethodEditor} />;
+  }
+
+  renderToolbar() {
+    return <View style={styles.toolbar} />;
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Status />
+        {this.renderMessageList()}
+        {this.renderToolbar()}
+        {this.renderInputMethodEditor()}
       </View>
     );
   }
@@ -16,8 +30,19 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white"
   },
+  content: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  inputMethodEditor: {
+    flex: 1,
+    backgroundColor: "white"
+  },
+  toolbar: {
+    borderTopWidth: 1,
+    borderTopColor: "rgba(0,0,0,0.04)",
+    backgroundColor: "white"
+  }
 });
